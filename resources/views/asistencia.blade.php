44beaf98fee8cae7e53f40b014fe3acfd9fecb95
marce ifran singh <ifranm962@gmail.com>
2023-09-13 10:12:44 -0300

    1

--- resources/views/asistencia.blade.php
+++ resources/views/asistencia.blade.php
@@ -63,7 +63,7 @@
       // Obtener las cámaras disponibles
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+          scanner.start(cameras[1]);
         } else {
           console.error('No cameras found.');
         }
