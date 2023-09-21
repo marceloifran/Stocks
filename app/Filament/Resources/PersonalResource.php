2b570a36b0716b177df0e4fc36c599b97d6ee666
marce ifran singh <ifranm962@gmail.com>
2023-09-20 22:36:00 -0300

    fix

--- app/Filament/Resources/PersonalResource.php
+++ app/Filament/Resources/PersonalResource.php
@@ -107,12 +107,12 @@ class PersonalResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make()
-                ->icon('heroicon-o-document')
-                ->label('299')
-                ->color('success')
-                // ->url(fn ($record) => route('personal.edit', ['record' => $record]))
-                ->url(fn($record) => route('personal.exportPdf',['record' => $record]))
+                // Tables\Actions\EditAction::make()
+                // ->icon('heroicon-o-document')
+                // ->label('299')
+                // ->color('success')
+                // // ->url(fn ($record) => route('personal.edit', ['record' => $record]))
+                // ->url(fn($record) => route('personal.exportPdf',['record' => $record]))
                 // Tables\Actions\DeleteAction::make(),
                 // Actions\CreateAction::make('299')->url(fn() => route('personal.exportPdf',['record' => $this->record]))->icon('heroicon-o-document')->label('299'),
 
