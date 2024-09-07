8ee59f4b4778b592839acb1f430135d700724d10
marce ifran <ifranm962@gmail.com>
2024-09-07 15:05:25 -0300

    cambio de logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{asset('/images/logo.webp')}}"  alt="logo" class="h-10" style="border-radius: 10px">
+<img src="{{asset('/images/2.png')}}"  alt="logo" class="h-10" style="border-radius: 10px">
